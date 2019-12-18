7344e272f65a3b79665048c3432a3c543707789a
unknown <13269512922@163.com>
2019-12-18 20:03:55 +0800

    安装elementUI

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUl from 'element-ui' // 引入包
+import 'element-ui/lib/theme-chalk/index' // 引入element样式
+import '../src/styles/index' // 引入初始化设置
+Vue.use(ElementUl) // 全局注册
 Vue.config.productionTip = false
 
 new Vue({

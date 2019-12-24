6ac659688e0fcf32374b41e8149c6ac19d728953
unknown <13269512922@163.com>
2019-12-24 16:52:45 +0800

    头部布局完成

---
+++ src/components/Home/layout-header.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- 头部组件 -->
+     <el-row class="layout-header" align="middle" type="flex" justify="space-between">
+
+         <el-col class="left" :span="6">
+             <i class="el-icon-s-unfold "></i>
+             <span class="title">江苏传智播客教育科技股份有限公司</span>
+         </el-col>
+
+         <el-col class="right" :span="4">
+           <el-row type="flex" justify="end" align="middle">
+               <img src="../../assets/img/ba.jpg" alt="">
+               <!-- 下拉菜单 -->
+               <el-dropdown>
+                   <span>wzkzl</span>
+                   <!-- 下拉菜单  具名插槽 -->
+                   <el-dropdown-menu slot="dropdown">
+                     <el-dropdown-item>个人信息</el-dropdown-item>
+                     <el-dropdown-item>Git地址</el-dropdown-item>
+                     <el-dropdown-item>退出</el-dropdown-item>
+                   </el-dropdown-menu>
+               </el-dropdown>
+           </el-row>
+         </el-col>
+     </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+ .layout-header{
+     height:60px;
+    .left{
+        font-size: 20px;
+        .title{
+            margin-left: 6px;
+            color:#2c3e50;
+        }
+    }
+    .right
+    {
+        img{
+            width:40px;
+            height: 40px;
+            border-radius: 50%;
+        }
+    }
+ }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,12 +2,15 @@
     <!-- 放置一个外层容器 -->
      <el-container>
       <!-- 左右布局 -->
-      <el-aside>左侧</el-aside>
+      <el-aside style="background-color:#353b4e; height:100vh;width:230px" >
+        <layout-aside></layout-aside>
+      </el-aside>
       <!-- 在放置一个container -->
       <el-container>
         <!-- 上下布局 -->
        <el-header>
-       头部
+       <!-- 头部组件 -->
+       <layout-header></layout-header>
        </el-header>
         <el-main>
              主要区域
@@ -17,8 +20,14 @@
 </template>
 
 <script>
-export default {
 
+import layoutAside from '../../components/Home/layout-aside'
+import layoutHeader from '../../components/Home/layout-header'
+export default {
+  components: {
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
+  }
 }
 </script>
 
